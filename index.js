f9043b0eac47cc4cd787591464bea5efcd2cf178
tlamidi <tlamidi@yahoo.co.uk>
2022-03-15 10:16:52 +0000

    add font to the style sheet

---
+++ index.js
@@ -0,0 +1,11 @@
+let user = true;
+let emailVerified = false
+if (user == true) {
+    if (emailVerified == false) {
+        console.log("Welcome to our web-site");
+    } else {
+        console.log("Please verify your email address");
+    }
+} else {
+    console.log("You have to be a member to log on to our web site");
+}
