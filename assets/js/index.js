7df3a4185956e8552bda71bba67fa95418e46a2d
tlamidi <tlamidi@yahoo.co.uk>
2022-04-22 23:15:50 +0000

    add a function and if statement to the js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
-    
+    function showTime() {
+        let date = new Date();
+        let h = date.getHours();
+        let m = date.getMinutes();
+        let s = date.getSeconds();
+        let period = 'AM';
 
+        if (h == 0) {
+            h = 12;
+        }
 
-            function showTime() {
-                let date = new Date();
-                let h = date.getHours();
-                let m = date.getMinutes();
-                let s = date.getSeconds();
-                let period = 'AM';
+        if (h > 12) {
+            h = h - 12;
+            period = "PM";
+        }
 
-                if (h == 0) {
-                    h = 12;
-                }
+        h = (h < 10) ? "0" + h : h;
+        m = (m < 10) ? "0" + m : m;
+        s = (s < 10) ? "0" + s : s;
+        let time = h + ":" + m + ":" + s + "" + period;
 
-                if (h > 12) {
-                    h = h - 12;
-                    period = "PM";
-                }
+        document.getElementById("MyClockDisplay").innerText =
+            time;
+        document.getElementById("MyClockDisplay").textContent =
+            time;
 
-                h = (h < 10) ? "0" + h : h;
-                m = (m < 10) ? "0" + m : m;
-                s = (s < 10) ? "0" + s : s;
+        setTimeout(showTime, 1000);
 
-                ]
-                let time = h + ":" + m + ":" + s + "" + period;
-                document.getElementById("MyClockDisplay").innerText =
-                    time;
-                document.getElementById("MyClockDisplay").textContent =
-                    time;
+    }
 
-                setTimeout(showTime, 1000);
+    showTime();
+})
 
-            }
-
-            showTime();
+function  myClockDisplay() {
+    if (body.style.backgroundColor = "orange") {
+        body.style.backgroundColor =  "green";
+    }else {
+        body.style.backgroundColor = "orange";
+    }
+}
