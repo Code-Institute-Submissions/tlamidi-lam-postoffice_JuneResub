42d376b36939aba21f58aa4874713e107b311da9
tlamidi <tlamidi@yahoo.co.uk>
2022-06-09 11:01:37 +0000

    add commenttco the coding on jvascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function showTime() {
+    function showTime() {      //function to determine settings, from day to seconds.
         let date = new Date();
         let h = date.getHours();
         let m = date.getMinutes();
         let s = date.getSeconds();
         let period = 'AM';
-
+        // An if statement determining first 12 hour count as AM
         if (h == 0) {
             h = 12;
         }
-
+        // An if statement for another 12 hour count as PM
         if (h > 12) {
             h = h - 12;
             period = "PM";
         }
-
+        // format for the clock display at the terminal
         h = (h < 10) ? "0" + h : h;
         m = (m < 10) ? "0" + m : m;
         s = (s < 10) ? "0" + s : s;
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
         setTimeout(showTime, 1000);
 
     }
-
+    // calling the showtime function
     showTime();
 })
 
